ce3e3273b453454a646b3e302c7172cc2ca6a33f
Allony Forever <goten0539@gmail.com>
2026-03-11 12:13:21 +0300

    fix-feature

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Calculator")
+    
+    a = int(input("First number: "))
+    b = int(input("Second number: "))
+    
+    print("Sum:", add(a, b))
+    print("Subtract:", subtract(a, b))
